fffbce7adb79de07f2dc152633da9588a3115f23
System Administrator <admin@example.com>
2019-09-30 14:40:10 +0530

    Changes by 'System Administrator' on 'SV_Devops_Test 2' -- Build #1

---
+++ sn/Global/Fix Script/active_true/DevopsTest.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-09-30 09:07:12
+ * Created By  : admin
+ * Updated On  : 2019-09-30 09:08:17
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=411322481bd080509614ff37dc4bcbf5
+ */
+gs.log("Hey Devops12");
